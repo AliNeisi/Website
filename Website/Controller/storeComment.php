9f0f8c0997fd94a28d971bd7b44300c4c20cc31f
Bandit <88215204+AliNeisi@users.noreply.github.com>
2021-09-19 21:44:20 +0430

    Delete Website directory

--- Website/Controller/storeComment.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (isset($_POST['storeComment'])) {
-    include "Config.php";
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $msg = $_POST['msg'];
-    $config = new Config();
-    $config->storeComment($name, $email, $msg);
-} else header('location:/My Personal Website/');
